a95bfbaf90e59559c3b3d790fd113578147efad6
kevin <kk60607kk@gmail.com>
2025-05-16 03:13:33 +0800

    switch 撰寫功能選單

--- test.js
+++ test.js
@@ -17,8 +17,9 @@ for(let i=1;i <= 9 ;i++){
 }
 document.write("</table>")
 */
-
+/*
 //使用 prompt 讓使用者輸入年齡，顯示「成年 / 未成年」。
+
 let age = prompt("請輸入年齡:");
 age = parseInt(age);
 
@@ -29,4 +30,23 @@ if (isNaN(age)) {
   alert("未成年");
 } else if (age >= 18) {
   alert("成年");
+}
+*/
+
+//使用 switch 撰寫一個功能選單（輸入 1～3 執行不同功能）。
+
+let num = prompt("請輸入1~3");
+num = parseInt(num);
+switch (num) {
+  case 1:
+    alert("我");
+    break;
+  case 2:
+    alert("你");
+    break;
+  case 3:
+    alert("他");
+    break;
+  default:
+    alert("請輸入有效數字！");
 }
