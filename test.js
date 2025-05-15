20d0dc046935ce1b70ef65df7f6f4b942c998684
kevin.w <kk60607kk@gmail.com>
2025-05-15 17:42:00 +0800

    九九乘法表+表格

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-console.log(test)
-console.log(test2)
+let a = 1
+let b = 1
+
+document.write("<table border = 1>")
+for(let i=1;i <= 9 ;i++){
+    document.write("<tr>")
+    for(let j=1 ;j <= 9;j++){
+        document.write("<td>")
+        document.write(`${a} * ${b} = ${a*b}`)
+        b++;
+        document.write("</td>")
+    };
+    a++;
+    b = 1;
+    document.write("</tr>")   
+}
+document.write("</table>")
