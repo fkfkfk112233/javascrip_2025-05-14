67828977bfc45178b70ab4f8e394ad4beb1c1c42
kevin <kk60607kk@gmail.com>
2025-05-16 03:01:29 +0800

    判斷成年/未成年

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 let a = 1
 let b = 1
 
@@ -15,3 +16,17 @@ for(let i=1;i <= 9 ;i++){
     document.write("</tr>")   
 }
 document.write("</table>")
+*/
+
+//使用 prompt 讓使用者輸入年齡，顯示「成年 / 未成年」。
+let age = prompt("請輸入年齡:");
+age = parseInt(age);
+
+if (isNaN(age)) {
+  alert("請輸入數字！");
+  //prompt("請輸入年齡:");
+} else if (age < 18) {
+  alert("未成年");
+} else if (age >= 18) {
+  alert("成年");
+}
